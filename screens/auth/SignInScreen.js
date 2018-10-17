b70a4b1b4a8775ad78d634c0b1e1ee025185a9d9
Alek Armbruster <alekarmbruster@gmail.com>
2018-10-17 13:11:34 +0200

    Http and Auth services implemented

--- screens/auth/SignInScreen.js
+++ screens/auth/SignInScreen.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import { AsyncStorage, StyleSheet, View, Button } from 'react-native';
+import { AsyncStorage, StyleSheet, View, Button, TextInput } from 'react-native';
 import PropTypes from 'prop-types';
+import I18n from '../../i18n';
+import authService from '../../services/AuthService';
 
 export default class SignInScreen extends React.Component {
   static navigationOptions = {
     title: 'Please sign in'
   };
 
-  static propTypes = {
-    navigation: PropTypes.object
+  state = {
+    email: '',
+    password: ''
   };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Button title="Sign in!" onPress={this._signInAsync} />
-        <Button title="Sign up!" onPress={this._goToSignUp} />
-      </View>
-    );
-  }
-
   _signInAsync = async () => {
+    const signinData = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    // const response = await authService.login(signinData);
     await AsyncStorage.setItem('userToken', 'abc');
     this.props.navigation.navigate('Main');
   };
@@ -28,7 +27,36 @@ export default class SignInScreen extends React.Component {
   _goToSignUp = () => {
     this.props.navigation.navigate('SignUp');
   };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder={I18n.t('auth.enterEmail')}
+          onChangeText={email => this.setState({ email })}
+          value={this.state.email}
+        />
+
+        <TextInput
+          secureTextEntry={true}
+          placeholder={I18n.t('auth.enterPass')}
+          onChangeText={password => this.setState({ password })}
+          value={this.state.password}
+        />
+
+        <Button title="Sign in!" onPress={this._signInAsync} />
+        <Button title="Sign up!" onPress={this._goToSignUp} />
+      </View>
+    );
+  }
 }
+
+SignInScreen.propTypes = {
+  navigation: PropTypes.object
+};
 
 const styles = StyleSheet.create({
   container: {
