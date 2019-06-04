3dffe1cda48af6e1a9dbb41c51732ecb4100c7fe
AlexSensei <antishadow15@gmail.com>
2019-06-04 14:34:47 +0200

    Fixed modal code

--- components/shared/modal/ErrorModal.js
+++ components/shared/modal/ErrorModal.js
@@ -3,15 +3,12 @@ import { TouchableOpacity, Text } from 'react-native';
 import PropTypes from 'prop-types';
 import $t from 'i18n';
 
-import ModalWrapper from './modalParts/Modal';
-import ModalHeader from './modalParts/ModalHeader';
-import ModalBody from './modalParts/ModalBody';
-import ModalFooter from './modalParts/ModalFooter';
+import { Modal, ModalHeader, ModalBody, ModalFooter } from './baseModal';
 import NavigationService from '../../../services/NavigationService';
 
 const ErrorModal = ({ isVisible, closeModal }) => {
   return (
-    <ModalWrapper isVisible={isVisible} onRequestClose={closeModal}>
+    <Modal isVisible={isVisible} onRequestClose={closeModal}>
       <ModalHeader>
         <Text>{$t('error.somethingWrong')}</Text>
       </ModalHeader>
@@ -26,7 +23,7 @@ const ErrorModal = ({ isVisible, closeModal }) => {
           <Text>{$t('error.restart')}</Text>
         </TouchableOpacity>
       </ModalFooter>
-    </ModalWrapper>
+    </Modal>
   );
 };
 
