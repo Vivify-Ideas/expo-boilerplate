62e73d72aa2e29b756c293538e354e0703a9582d
Md. Al-Amin <alaminopu.me@gmail.com>
2019-10-03 00:36:40 +0600

    Fix formatting of babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,35 +1,34 @@
 module.exports = function bpe(api) {
-//   api.cache(true);
-    const presets = [
+  //   api.cache(true);
+  const presets = [
     'babel-preset-expo',
     'module:metro-react-native-babel-preset',
     'module:react-native-dotenv'
-    ];
-    const plugins = [];
+  ];
+  const plugins = [];
 
-    const envDevelopment = {
+  const envDevelopment = {
     presets: presets,
     plugins: [
-        '@babel/transform-react-jsx-source',
-        [
+      '@babel/transform-react-jsx-source',
+      [
         'module-resolver',
         {
-            'root': './',
-            'alias': {
-            'i18n': './i18n'
-            }
+          root: './',
+          alias: {
+            i18n: './i18n'
+          }
         }
-        ]
+      ]
     ]
-    };
+  };
 
-    if(api.env(['development', 'test'])){
+  if (api.env(['development', 'test'])) {
     return envDevelopment;
-    }
+  }
 
-    return {
+  return {
     presets: presets,
-    plugins: plugins,
-    };
+    plugins: plugins
+  };
 };
-    
